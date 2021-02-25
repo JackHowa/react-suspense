016025363adda727e49069fa9cbb98905d883327
jackhowa <jack.howard@washpost.com>
2021-02-24 22:17:41 -0600

    Setup basic fetch with suspense and promise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,52 +2,45 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import { fetchPokemon, PokemonDataView } from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+let pokemon;
 
-// 🐨 create a variable called "pokemon" (using let)
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+// define the promise
+// set the defined value above within the success handler 
+const pokeMonPromise = fetchPokemon('pikachu').then(resolvedValue => (pokemon = resolvedValue))
 
 function PokemonInfo() {
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
-
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        <img src={pokemon.image} alt={pokemon.name} />
+
+  // if pokemon has been set 
+  // then render
+  if (pokemon) {
+    return (
+      <div>
+        <div className="pokemon-info__img-wrapper">
+          <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+        <PokemonDataView pokemon={pokemon} />
       </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
+    )
+  }
+
+  // if fetching
+  // then render suspense fallback loading
+  throw pokeMonPromise;
 }
 
 function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <React.Suspense fallback={<div>Fetching pikachu</div>}>
+          <PokemonInfo />
+        </React.Suspense>
       </div>
     </div>
   )
